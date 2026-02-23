6d788d6b62a796af71a5046e9f153552462910e6
Suraj <2405244@live.stmarys.ac.uk>
2026-02-23 15:30:57 +0000

    Added API service with Axios instance and JWT token interceptor(this helps handling api calls and error )

---
+++ linear/frontend/src/services/index.ts
@@ -0,0 +1,4 @@
+export { authService } from './authService';
+export { moodService } from './moodService';
+export { insightService } from './insightService';
+export { default as api } from './api';
