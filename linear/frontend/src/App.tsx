7da78a97622b783b044e0b27e563898db56cccc1
Suraj <2405244@live.stmarys.ac.uk>
2026-02-25 22:34:08 +0000

    added new routes for Tools like , Inbox, History, and Insights in App component this allow for freedom use for linear

--- linear/frontend/src/App.tsx
+++ linear/frontend/src/App.tsx
@@ -1,15 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context';
 import { Layout, ProtectedRoute } from './components';
-import { Home, Login, Register, Dashboard, NotFound } from './pages';
+import { Home, Login, Register, Dashboard, Tools, Inbox, History, Insights, NotFound } from './pages';
 
 const App: React.FC = () => (
   <AuthProvider>
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          {/* Public routes */}
+          {/* Main tabs */}
           <Route path="/" element={<Home />} />
+          <Route path="/tools" element={<Tools />} />
+          <Route path="/inbox" element={<Inbox />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/insights" element={<Insights />} />
+
+          {/* Auth */}
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
 
