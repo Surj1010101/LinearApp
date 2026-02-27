3f6120a67280d6612f22656ea2e454fea95e81bd
Ntanelio <2407223@live.stmarys.ac.uk>
2026-02-27 23:28:52 +0000

    Creating initial natural language processing model using hugging face, using nlp_fine_tuning.csv to fine tune and train the AI to give appropriate sentiment scores. nlp_test.py used for testing responses.

--- nlp_test.py
+++ nlp_test.py
@@ -1,13 +1,7 @@
 from transformers import pipeline
 
-sentiment_ai = pipeline('sentiment-analysis')
+burnout_ai = pipeline('text-classification', model = "./burnout_nlp_model", tokenizer = "./burnout_nlp_model")
 
-def get_text_score(user_text):
-    raw_result = sentiment_ai(user_text)
-    data = raw_result[0]
-    label = data['label']
-    score = data['score']
-    if label == "NEGATIVE": return score * -1
-    else: return score
-
-print(get_text_score('I feel tired, but i got a lot of things done'))
+#text = 'I managed to complete everything but felt very stressed and burned out'
+text = 'I hate how much workload gets dumped on me recently, its starting to stress me out'
+print(burnout_ai(text))
---
+++ train_nlp.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments
+from datasets import Dataset
+
+#sources:https://huggingface.co/docs/transformers/en/training
+#https://www.youtube.com/watch?v=QEaBAZQCtwE&t=1s
+
+#Fetching the dataset for AI tuning
+filepath = "C:\\Users\danel\PycharmProjects\AI-Training-Model-Linear-App\Dataset\\nlp_fine_tuning.csv"
+data = pd.read_csv(filepath)
+data = data.dropna()
+
+hugging_face_dataset = Dataset.from_pandas(data)
+#Renaming column sentiment score to label so it can be found by the ai
+hugging_face_dataset = hugging_face_dataset.rename_column('sentiment_score', 'labels')
+
+#Initialising AI
+model_name = "distilbert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+#loading model and providing expected labels (1 as it will be the sentiment score)
+model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=1)
+
+#Tokenisation rule
+def tokenize(examples):
+    return tokenizer(examples["journal_entry"], padding="max_length", truncation=True)
+#Apply rule to dataset
+tokenized_dataset = hugging_face_dataset.map(tokenize, batched=True)
+
+
+train_dataset = tokenized_dataset
+#set training parameters
+training_args = TrainingArguments(
+    output_dir='./results',
+    num_train_epochs=3,
+    per_device_train_batch_size=2,
+)
+#create the trainer
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+)
+#start training
+trainer.train()
+
+trainer.save_model("./burnout_nlp_model")
+tokenizer.save_pretrained("./burnout_nlp_model")
+print("Model saved")
