7db37c486e8092a9f95146ed291079bd04e2b8af
Ntanelio <2407223@live.stmarys.ac.uk>
2026-03-10 00:41:36 +0000

    Expanding nlp model to output scores for burnout, stress, productivity, engagement, nutrition and activity.

--- train_nlp.py
+++ train_nlp.py
@@ -8,19 +8,30 @@ from datasets import Dataset
 
 #Fetching the dataset for AI tuning
 #filepath = "C:\\Users\danel\PycharmProjects\AI-Training-Model-Linear-App\Dataset\\nlp_fine_tuning.csv"
-filepath = "C:\\Users\danel\PycharmProjects\AI-Training-Model-Linear-App\Dataset\\nlp_fine_tuning.csv"
+filepath = "Dataset\\advanced_synthetic_nlp_dataset.csv"
 data = pd.read_csv(filepath)
 data = data.dropna()
 
 hugging_face_dataset = Dataset.from_pandas(data)
 #Renaming column sentiment score to label so it can be found by the ai
-hugging_face_dataset = hugging_face_dataset.rename_column('sentiment_score', 'labels')
+#hugging_face_dataset = hugging_face_dataset.rename_column('sentiment_score', 'labels')
 
+#packing labels into a batch
+def bundle(batch):
+    #combining 6 columns row by row into a list
+    batch["labels"] = [
+        [b, s, p, e, n, a] for b, s, p, e, n, a in zip(
+            batch["burnout_score"],batch["stress_score"], batch["productivity_score"], batch["engagement_score"], batch["nutrition_score"], batch["activity_score"])
+    ]
+    return batch
+hugging_face_dataset = hugging_face_dataset.map(bundle, batched=True)
 #Initialising AI
 model_name = "distilbert-base-uncased"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 #loading model and providing expected labels (1 as it will be the sentiment score)
-model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=1)
+#using regression as we are expecting continuous number to be predicted
+#num labels set to 6 instead of previous 1 as inputs increased
+model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=6, problem_type="regression")
 
 #Tokenisation rule
 def tokenize(examples):
