ef97755cf22bfba0bd7c93d41da2dc61e52e72d8
Your Name <kirthanloi@icloud.com>
2026-03-14 19:44:43 +0000

    Bug fix

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:password@localhost/linear_db"
+DATABASE_URL = "sqlite:///./linear.db"
 
 engine = create_engine(DATABASE_URL)
 
